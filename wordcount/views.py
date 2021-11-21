013e9894dd38e15c7d28251a16172c13a14efc79
Kunal Kumar <kunalkrate1973@gmail.com>
2021-11-21 11:31:55 +0530

    changes applied

--- wordcount/views.py
+++ wordcount/views.py
@@ -6,4 +6,18 @@ def homepage(request):
 
 
 def count(request):
-    return render(request, 'count.html')
+    fulltext = request.GET['fulltext']
+
+    wordlist = fulltext.split()
+
+    word_dictionary = {}
+
+    for word in wordlist:
+        if word in word_dictionary:
+            #Increase
+            word_dictionary[word] += 1
+        else:
+            #add to the worddictionary
+            word_dictionary[word] = 1
+
+    return render(request, 'count.html',{'fulltext':fulltext,'count':len(wordlist),'word_dictionary':word_dictionary.items()})
