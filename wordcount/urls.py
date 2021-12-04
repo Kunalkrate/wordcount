a36b010e8e3c33fc89e4d0161d8a914d5aac781a
Kunal Kumar <kunalkrate1973@gmail.com>
2021-12-04 16:05:43 +0530

    first commit

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.homepage),
+    path('', views.homepage,name="hello"),
+    path('home/',views.home,name="home"),
     path('count/',views.count, name='count'),
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 def homepage(request):
+    return render(request, 'index.html')
+
+def home(request):
     return render(request, 'home.html')
 
 
