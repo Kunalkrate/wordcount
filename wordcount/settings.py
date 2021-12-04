af7059b2ea8e20c469a1dfda2a15e13afb1eed5c
Kunal Kumar <kunalkrate1973@gmail.com>
2021-12-04 18:40:24 +0530

    first commit

--- wordcount/settings.py
+++ wordcount/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&7oj9j#j6$_p$9fqexom-%=)h%fu0t0fo*yd0=3w$xkts-2!m='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- wordcount/urls.py
+++ wordcount/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 
 from django.urls import path
 from . import views
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('', views.homepage,name="hello"),
     path('home/',views.home,name="home"),
     path('count/',views.count, name='count'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
